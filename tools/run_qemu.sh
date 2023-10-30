1d65f1e84fb6c2060a852315db50fa829c61da75
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-30 20:11:11 +0200

    [tools:run_qemu] Build | QEMU simulation for STM32F411RE

---
+++ tools/run_qemu.sh
@@ -0,0 +1,7 @@
+_HERE=${BASH_SOURCE[0]}
+_HERE=`dirname $_HERE`
+_HERE=`realpath $_HERE`
+IMAGE_PATH=$_HERE/../build/src/common/application.bin
+
+qemu-system-gnuarmeclipse -board NUCLEO-F411RE -image $IMAGE_PATH -gdb tcp::3333 -S -nographic
+
