f38318cb85239ad732eeb8f79d1625210ddb1f2b
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-30 21:06:11 +0200

    [common:target] Bug | Build system fix

---
+++ src/common/target.h
@@ -0,0 +1,15 @@
+
+//
+// target.h
+//
+// Created on: 2023-10-30
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
+//
+
+#ifndef COMMON_TARGET_H_
+#define COMMON_TARGET_H_
+
+/// \brief performs basic clock initialization, brings up peripherals
+void targetInitializeClock();
+
+#endif  // COMMON_TARGET_H_
