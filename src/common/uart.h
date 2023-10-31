2717dcdc7ec006e6ea95d6be47e2c4837d7e1ed0
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-31 22:01:53 +0200

    [common:uart] Impl | Extended API

--- src/common/uart.h
+++ src/common/uart.h
@@ -20,9 +20,25 @@ enum UartStopBitLength {
 	UartStopBitLength0p5,
 };
 
+/// \param `aNextChar` -- the next UART character
+/// \returns 1, if the next character is valid (the buffer was not empty). 0
+/// otherwise
+typedef int (*UartIsrHook)(unsigned char *aOutNextChar);
+
 /// \brief Performs UART initialization
 /// \param `aUartWordLength`: see `UartWordLength`
 /// \param `aUartStopBitLength`: see `UartStopBitLength`
 void uartInitialize(unsigned long aBaudrate, unsigned aUartWordLength, unsigned aUartStopBitLength);
+
+/// \brief Initializes static UART ISR hook
+void uartSetIsrHook(UartIsrHook aUartIsrHook);
+
+/// \brief Enables UART transmission. May be called from either exception or
+/// threading mode
+void uartEnableFromIsr();
+
+/// \brief Disables UART transmissions. May be called from either exception or
+/// threading mode
+void uartDisableFromIsr();
 
 #endif // COMMON_UART_H_
