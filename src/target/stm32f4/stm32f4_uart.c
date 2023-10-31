2f7294f5ec2e241aba28947c435b2f40200be4dd
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-31 21:25:04 +0200

    [stm32f4:stm32f4_uart] Impl | UART configuration: Baudrate, word length, stop bits

--- src/target/stm32f4/stm32f4_uart.c
+++ src/target/stm32f4/stm32f4_uart.c
@@ -13,7 +13,46 @@
 /// \brief Initializes UART2
 /// \pre Clocking is supposed to be initialized
 /// \pre UART2 is supposed to be enabled in CCR
-unsigned uartInitialize(int aBaudrate, unsigned aUse9BitWord, unsigned aUse2StopBits)
+void uartInitialize(unsigned long aBaudrate, unsigned aUartWordLength, unsigned aUartStopBitLength)
 {
-	USART_TypeDef usart;
+	USART_TypeDef *usart = USART2;
+	usart->CR1 |= USART_CR1_TXEIE;  // Enable "TX empty" interrupt
+
+	// Configure word length
+	if (aUartWordLength == UartWordLength9Bit) {
+		usart->CR1 |= USART_CR1_M;
+	}
+
+	// Configure the number of stop bits
+	switch (aUartStopBitLength) {
+		case UartStopBitLength0p5:  // See RM0402, Rev. 6, p. 806
+			usart->CR2 |= USART_CR2_STOP_0;
+
+			break;
+
+		case UartStopBitLength1:
+			break;
+
+		case UartStopBitLength1p5:
+			usart->CR2 |= (USART_CR2_STOP_0 | USART_CR2_STOP_1);
+
+			break;
+
+		case UartStopBitLength2:
+			usart->CR2 |= USART_CR2_STOP_1;
+
+			break;
+	}
+
+	// Configure fractional baudate value, RM0402, Rev. 6, p. 772
+	{
+		const int useOversampling8 = 0; // TODO: optimize out, use defines
+		const float usartDivf = ((float)targetStm32f4GetUartClockFrequencyHz())
+			/ (8 * (2 - useOversampling8) * aBaudrate);
+		const unsigned long mantissa = (unsigned long)usartDivf;
+		const float fractionCoefficientMultiplierf = 16.0f;
+		const unsigned long long fraction = (unsigned long long)(fractionCoefficientMultiplierf
+			* (usartDivf - (float)mantissa));
+		usart->BRR = (mantissa << 4) | fraction;
+	}
 }
