5c40a6a7c78e990fc5a93fcccc60c9a6eb2938a7
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-02 19:36:48 +0300

    [common:random] Impl | Random number acquisition

---
+++ src/common/random.h
@@ -0,0 +1,14 @@
+//
+// random.h
+//
+// Created on: Nov 02, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#ifndef COMMON_RANDOM_H_
+#define COMMON_RANDOM_H_
+
+/// \brief Returns random U32
+unsigned long randomGetU32();
+
+#endif // COMMON_RANDOM_H_
