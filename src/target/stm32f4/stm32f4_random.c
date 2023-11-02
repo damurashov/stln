5bcf083dc1de97c627340b00cae9c39e8596dfd4
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-02 19:36:47 +0300

    [stm32f4:stm32f4_random] Impl | Random number acquisition

---
+++ src/target/stm32f4/stm32f4_random.c
@@ -0,0 +1,14 @@
+//
+// stm32f4_random.c
+//
+// Created on: Nov 02, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+// Implements common/random.h
+
+#include <stm32f412cx.h>
+
+unsigned long randomGetU32()
+{
+	return RNG->DR;  // RM0402 Rev 6 p. 411
+}
