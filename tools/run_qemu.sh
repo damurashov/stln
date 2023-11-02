c712a5ac12888d5bc24e162b88a119791d48bab0
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-02 20:03:28 +0300

    [tools:run_qemu] Build | Verbose flag

--- tools/run_qemu.sh
+++ tools/run_qemu.sh
@@ -3,5 +3,5 @@ _HERE=`dirname $_HERE`
 _HERE=`realpath $_HERE`
 IMAGE_PATH=$_HERE/../build/src/common/application.bin
 
-qemu-system-gnuarmeclipse -board NUCLEO-F411RE -image $IMAGE_PATH -gdb tcp::3333 -S -nographic
+qemu-system-gnuarmeclipse -board NUCLEO-F411RE -image $IMAGE_PATH -gdb tcp::3333 -S -nographic -verbose
 
