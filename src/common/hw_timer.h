995f71b1219f02e1f1416fc8de551e0393eef94c
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-03 12:02:59 +0300

    [common:hw_timer] Impl | HW timer API

---
+++ src/common/hw_timer.h
@@ -0,0 +1,22 @@
+//
+// hw_timer.h
+//
+// Created on: Nov 03, 2023
+//     Author: Dmitry Murashov (d <DOT> murashov <AT> geoscan <DOT> aero)
+//
+
+#ifndef COMMON_HW_TIMER_H_
+#define COMMON_HW_TIMER_H_
+
+/// Will be invoked each time the timer's interrupt gets triggered
+typedef void (*HwTimerIsrHook)();
+
+/// \brief Initializes timer w/ `HwTimerIsrHook` instance
+/// \pre The timer must be pre-initialized for a target
+void hwTimerSetIsrHook(HwTimerIsrHook aHwTimerIsrHook);
+
+/// \brief after # `anTicks` the `HwTimerIsrHook` will get triggered (if one
+/// will have been set by the time)
+void hwTimerSetTimeoutTicks(unsigned long anTicks);
+
+#endif // COMMON_HW_TIMER_H_
