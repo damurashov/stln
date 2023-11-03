658d746e6964c7d0bb5670606b4bacca8fd7eb26
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-03 18:13:46 +0300

    [stm32f4:stm32f4_timer] Impl | Implemented "timer" API

---
+++ src/target/stm32f4/stm32f4_timer.c
@@ -0,0 +1,23 @@
+//
+// stm32f4_timer.c
+//
+// Created on: Nov 03, 2023
+//     Author: Dmitry Murashov (d <DOT> murashov <AT> geoscan <DOT> aero)
+//
+
+#include "hw_timer.h"
+#include "stm32f4_timer.h"
+
+static HwTimerIsrHook sHwTimerIsrHook;
+
+void hwTimerSetIsrHook(HwTimerIsrHook aHwTimerIsrHook)
+{
+	sHwTimerIsrHook = aHwTimerIsrHook;
+}
+
+void hwTimerStartTimeoutTicks(unsigned long anTicks)
+{
+	volatile TIM_TypeDef *tim = stm32f4TimerGetTimTypedef();
+	tim->ARR = anTicks;  // Set the number of ticks
+	tim->CR1 |= TIM_CR1_CEN;  // Enable counting
+}
