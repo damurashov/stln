9d7c42af44074c100c1b549dc413f173d04c0279
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-03 18:19:00 +0300

    [stm32f4:stm32f4_hw_timer,stm32f4_timer] Ref | Naming

--- src/target/stm32f4/stm32f4_hw_timer.h
+++ src/target/stm32f4/stm32f4_hw_timer.h
@@ -1,12 +1,12 @@
 //
-// stm32f4_timer.h
+// stm32f4_hw_timer.h
 //
 // Created on: Nov 03, 2023
 //     Author: Dmitry Murashov (d <DOT> murashov <AT> geoscan <DOT> aero)
 //
 
-#ifndef TARGET_STM32F4_STM32F4_TIMER_H_
-#define TARGET_STM32F4_STM32F4_TIMER_H_
+#ifndef TARGET_STM32F4_STM32F4_HW_TIMER_H_
+#define TARGET_STM32F4_STM32F4_HW_TIMER_H_
 
 #include <stm32f412cx.h>
 
@@ -16,4 +16,4 @@ static inline TIM_TypeDef *stm32f4TimerGetTimTypedef()
 	return TIM2;
 }
 
-#endif // TARGET_STM32F4_STM32F4_TIMER_H_
+#endif // TARGET_STM32F4_STM32F4_HW_TIMER_H_
