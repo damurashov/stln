1d8abaacd7ac2a18e9705e5288ea1c0b4e494e2b
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-04 20:18:44 +0200

    [stm32f4:CMakeLists cmake:build_configuration] Build |

--- cmake/build_configuration.cmake
+++ cmake/build_configuration.cmake
@@ -5,6 +5,7 @@ set_property(GLOBAL PROPERTY STAP_ASMFLAGS ${LINKER_SCRIPT_PATH})
 set_property(GLOBAL PROPERTY STAP_LINKER_FLAGS ${LINKER_SCRIPT_PATH})
 set_property(GLOBAL PROPERTY STAP_TARGETS "")
 
+# Updates the global list of C flags
 macro(stap_build_configuration_append_cflag CFLAG)
 	message(STATUS "Adding CFLAG ${CFLAG}")
 	get_property(_TEMP GLOBAL PROPERTY STAP_CFLAGS)
@@ -12,6 +13,7 @@ macro(stap_build_configuration_append_cflag CFLAG)
 	set_property(GLOBAL PROPERTY STAP_CFLAGS ${_TEMP})
 endmacro()
 
+# Updates the global list of ASM flags
 macro(stap_build_configuration_append_asmflag asmflag)
 	message(STATUS "Adding ASM flag ${asmflag}")
 	get_property(_TEMP GLOBAL PROPERTY STAP_ASMFLAGS)
@@ -19,7 +21,7 @@ macro(stap_build_configuration_append_asmflag asmflag)
 	set_property(GLOBAL PROPERTY STAP_ASMFLAGS ${_TEMP})
 endmacro()
 
-
+# Updates the global list of linker flags
 macro(stap_build_configuration_append_linker_flag LINKER_FLAG)
 	message(STATUS "Adding linker flag `${LINKER_FLAG}`")
 	get_property(_TEMP GLOBAL PROPERTY STAP_LINKER_FLAGS)
@@ -27,6 +29,7 @@ macro(stap_build_configuration_append_linker_flag LINKER_FLAG)
 	set_property(GLOBAL PROPERTY STAP_LINKER_FLAGS ${_TEMP})
 endmacro()
 
+# Iterates over listed targets and sets compiler options for those
 macro(stap_build_configuration_apply_flags_to_targets)
 	get_property(_TEMP_STAP_TARGETS GLOBAL PROPERTY STAP_TARGETS)
 
@@ -37,7 +40,7 @@ macro(stap_build_configuration_apply_flags_to_targets)
 		get_property(_TEMP_LINKER_FLAGS GLOBAL PROPERTY STAP_LINKER_FLAGS)
 		string(JOIN " " _TEMP_CFLAGS ${_TEMP_CFLAGS})
 		string(JOIN " " _TEMP_ASMFLAGS ${_TEMP_ASMFLAGS})
-		string(JOIN " " _TEMP_LINKER_FLAGS ${_TEMP_LINKER_FLAGS})
+		string(JOIN "," _TEMP_LINKER_FLAGS ${_TEMP_LINKER_FLAGS})
 		message(STATUS "... C flags: ${_TEMP_CFLAGS}")
 		message(STATUS "... ASM flags: ${_TEMP_ASMFLAGS}")
 		message(STATUS "... Linker flags: ${_TEMP_LINKER_FLAGS}")
@@ -46,10 +49,11 @@ macro(stap_build_configuration_apply_flags_to_targets)
 			$<$<COMPILE_LANGUAGE:ASM>:SHELL:${_TEMP_ASMFLAGS}>
 		)
 		set_target_properties(${TARGET_NAME} PROPERTIES LINKER_LANGUAGE C)
-		target_link_options(${TARGET_NAME} PRIVATE "SHELL:${_TEMP_LINKER_FLAGS}")
+		target_link_options(${TARGET_NAME} PRIVATE "SHELL:-Wl,${_TEMP_LINKER_FLAGS}")
 	endforeach()
 endmacro()
 
+# Enqueues a target into the list of targets
 macro(stap_build_configuration_apply_flags_to_target TARGET_NAME)
 	get_property(_TEMP_STAP_TARGETS GLOBAL PROPERTY STAP_TARGETS)
 	list(APPEND _TEMP_STAP_TARGETS ${TARGET_NAME})
