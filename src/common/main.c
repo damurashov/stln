372122daadbf142d3a4ddd9799ce2768a237fb18
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-04 22:03:37 +0200

    [common:main] Impl | Application launch

--- src/common/main.c
+++ src/common/main.c
@@ -1,11 +1,17 @@
+#include "application.h"
 #include "memory.h"
 #include "target.h"
 
 int main(void)
 {
+	// System bringup
 	memoryInitializeBss();
 	memoryInitializeData();
 	targetUp();
+
+	// Run application
+	ApplicationHandle application = applicationInitialize();
+	applicationRun(application);
 
 	while (1) {
 	}
